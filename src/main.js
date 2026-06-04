22f771cce9de01b246734367db79da408f362e20
Kerem Demirgün <keremdemirgun@gmail.com>
2026-06-04 21:34:08 +0300

    fetch!

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import './style.css'
+const weatherResults = document.getElementById('weatherResults');
+const cityInput = document.getElementById('cityInput');
+const temp = document.getElementById('temp');
+const weatherIcon = document.getElementById('weatherIcon');
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+const errorMessage = document.getElementById("errorMessage");
+const errorSection = document.getElementById("errorSection");
+
+let lang = "tr"
+const API_KEY = "fee2409e94afe601cc737187d5b14da9";
+
+
+async function GetWeather(sehir) {
+
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${sehir}&appid=${API_KEY}&lang=${lang}&units=metric`);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    
+}
+
+getWeatherBtn.addEventListener("click", function() {
+    GetWeather(cityInput.value);
+});
+
+
+function showError(msg){
+    console.error(msg);
+    errorSection.classList.remove("hidden");
+    errorMessage.textContent = msg;
+}
+
