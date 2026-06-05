8007c653846ecd5c6cbc061a5f0badb22261e332
Kerem Demirgün <keremdemirgun@gmail.com>
2026-06-06 00:19:28 +0300

    some css

--- src/main.js
+++ src/main.js
@@ -6,33 +6,54 @@ const weatherIcon = document.getElementById('weatherIcon');
 const getWeatherBtn = document.getElementById("getWeatherBtn");
 const errorMessage = document.getElementById("errorMessage");
 const errorSection = document.getElementById("errorSection");
+const cityName = document.getElementById("cityName");
 
 let lang = "tr"
 const API_KEY = "fee2409e94afe601cc737187d5b14da9";
 
 
-async function GetWeather(sehir) {
+async function getWeather(sehir) {
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${sehir}&appid=${API_KEY}&lang=${lang}&units=metric`);
-    const data = await response.json();
-    
-    console.log(data);
-    showWeatherData(data);
+    if(cityInput.value.trim() == ""){
+        showError("Lütfen şehir kısmını boş bırakmayınız.");
+        
+    }
+    else{
+        weatherResults.classList.add("hidden");
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${sehir}&appid=${API_KEY}&lang=${lang}&units=metric`);
+        const data = await response.json();
+        
+        console.log(data);
+        showWeatherData(data);
+    }
+
+
 }
 
 getWeatherBtn.addEventListener("click", function() {
-    GetWeather(cityInput.value);
+    getWeather(cityInput.value);
 
 });
 
 
 function showWeatherData(data){
-    weatherResults.classList.remove("hidden");
+    if(data.message == "city not found"){
+        showError("Şehir bulunamadı.");
+    }
+    else{
+        errorSection.classList.add("hidden");
+        weatherResults.classList.remove("hidden");
+        cityName.textContent = data.name;
+        temp.textContent = data.main.feels_like + "°";
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    }
+
 
 
 }
 
 function showError(msg){
+    weatherResults.classList.add("hidden");
     console.error(msg);
     errorSection.classList.remove("hidden");
     errorMessage.textContent = msg;
