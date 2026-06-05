6da7ec7659922412bc66343150e9191126eca06e
Kerem Demirgün <keremdemirgun@gmail.com>
2026-06-05 18:45:48 +0300

    show

--- src/main.js
+++ src/main.js
@@ -13,20 +13,24 @@ const API_KEY = "fee2409e94afe601cc737187d5b14da9";
 
 async function GetWeather(sehir) {
 
-
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${sehir}&appid=${API_KEY}&lang=${lang}&units=metric`);
-
     const data = await response.json();
-
-    console.log(data);
-
     
+    console.log(data);
+    showWeatherData(data);
 }
 
 getWeatherBtn.addEventListener("click", function() {
     GetWeather(cityInput.value);
+
 });
 
+
+function showWeatherData(data){
+    weatherResults.classList.remove("hidden");
+
+
+}
 
 function showError(msg){
     console.error(msg);
